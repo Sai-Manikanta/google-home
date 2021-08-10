f020420448f68e8bd4c338e84a8dfab72f7feb46
mani333007 <mani333007@gmail.com>
2021-08-10 17:57:19 +0530

    some fixes

--- src/components/Suggetions.js
+++ src/components/Suggetions.js
@@ -2,8 +2,9 @@ function Suggetions({ list, setSearchQuery, setIsQueryFromSuggs }) {
     return (
         <div className="bg-white rounded-b absolute w-full" style={{ top: '40px' }}>
             <ul>
-                {list.map(sugg => (
+                {list.map((sugg, index) => (
                     <li 
+                        key={index}
                         className="px-2 py-1.5 text-sm border-t cursor-pointer"
                         onClick={() => {
                             setSearchQuery(sugg.value);
